b79903cec3c903bf7255ad68830f5585425d16f6
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-11-02 20:01:55 -0500

    Added: todo Notes

---
+++ app/src/main/java/com/nullcognition/datastructuresproblemsolvingusingjava/Notes.java
@@ -0,0 +1,8 @@
+package com.nullcognition.datastructuresproblemsolvingusingjava;
+// ersin 02/11/15 Copyright (c) 2015+ All rights reserved.
+
+
+public class Notes{/*
+
+Todo recursion, bitwise low level algos, graphs, trees in general, etc.
+*/}
